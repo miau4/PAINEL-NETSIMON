610be78740666c5007628cf4f9ddd6317d9bafd3
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 04:52:57 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+clear
+
+echo "XRAY MANAGER"
+
+echo "1 Criar usuario"
+echo "2 Remover usuario"
+echo "3 Usuarios online"
+echo "4 Rebuild config"
+echo "0 Sair"
+
+read op
+
+case $op in
+
+1)
+adduser
+;;
+
+2)
+deluser
+;;
+
+3)
+online
+;;
+
+4)
+rebuild
+;;
+
+esac
