77bfe5ce1af4cf79e08905f888620db4332903b9
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 10:20:47 -0300

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 CONFIG="/etc/xray/config.json"
 USERS="/etc/xray-manager/users.xray"
 KEYFILE="/etc/xray-manager/reality.key"
@@ -7,7 +6,11 @@ KEYFILE="/etc/xray-manager/reality.key"
 # Criar arquivo de usuários se não existir
 [ ! -f "$USERS" ] && touch "$USERS"
 
-read -p "Nome do usuario: " user
+# Solicitar dados
+read -p "Nome do usuário: " user
+read -sp "Senha: " password
+echo ""
+read -p "Validade (dias): " validade
 
 UUID=$(uuidgen)
 IP=$(curl -s ifconfig.me)
@@ -15,36 +18,34 @@ PUBLIC=$(grep PUBLIC $KEYFILE | cut -d= -f2)
 SNI=$(jq -r '.inbounds[2].streamSettings.realitySettings.dest' $CONFIG)
 SID="6ba85179e30d4fc2"
 
+# Calcular data de expiração
+EXP=$(date -d "+$validade days" +"%Y-%m-%d")
+
 # Adicionar nos inbounds
-jq ".inbounds[1].settings.clients += [{\"id\":\"$UUID\",\"email\":\"$user\"}]" $CONFIG > /tmp/config.json
-mv /tmp/config.json $CONFIG
-jq ".inbounds[2].settings.clients += [{\"id\":\"$UUID\",\"email\":\"$user\"}]" $CONFIG > /tmp/config.json
-mv /tmp/config.json $CONFIG
-jq ".inbounds[3].settings.clients += [{\"id\":\"$UUID\",\"email\":\"$user\"}]" $CONFIG > /tmp/config.json
-mv /tmp/config.json $CONFIG
+for i in 1 2 3; do
+  jq ".inbounds[$i].settings.clients += [{\"id\":\"$UUID\",\"email\":\"$user\"}]" $CONFIG > /tmp/config.json
+  mv /tmp/config.json $CONFIG
+done
 
 # Salvar no arquivo exclusivo
-echo "$user:$UUID" >> $USERS
+echo "$user:$password:$UUID:$EXP" >> $USERS
+
+# Criar usuário SSH com a mesma senha
+useradd -M -s /bin/false "$user" 2>/dev/null
+echo "$user:$password" | chpasswd
 
 # Reiniciar Xray
 systemctl restart xray
 
 # Mostrar links
-echo ""
-echo -e "Usuario criado: ${GREEN}$user${NC}"
+echo -e "\nUsuário criado com sucesso!"
+echo "Nome: $user"
+echo "Senha: $password"
 echo "UUID: $UUID"
+echo "Validade: $EXP"
 echo ""
-echo -e "${YELLOW}LINK VLESS XHTTP:${NC}"
+echo "LINK VLESS XHTTP:"
 echo "vless://$UUID@$IP:443?encryption=none&type=xhttp&security=tls&path=/#${user}"
 echo ""
-echo -e "${YELLOW}LINK VLESS REALITY:${NC}"
-echo "vless://$UUID@$IP:8443?encryption=none&security=reality&type=tcp&sni=$SNI&fp=chrome&pbk=$PUBLIC&sid=$SID&flow=xtls-rprx-vision#$user"
-echo ""
-echo -e "${YELLOW}LINK VLESS WS:${NC}"
-echo "vless://$UUID@$IP:80?type=ws&path=/vless&security=none#$user"
-echo ""
-echo -e "${YELLOW}LINK VMESS WS:${NC}"
-echo "vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"$user\",\"add\":\"$IP\",\"port\":\"8080\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"\",\"path\":\"/vmess\",\"tls\":\"\"}" | base64 -w0)"
-echo ""
-echo -e "${YELLOW}LINK VLESS TCP:${NC}"
-echo "vless://$UUID@$IP:8880?type=tcp&security=none#$user"
+echo "LINK VLESS REALITY:"
+echo "vless://$UUID@$IP:443?encryption=none&security=reality&type=tcp&sni=$SNI&fp=chrome&pbk=$PUBLIC&sid=$SID&flow=xtls-rprx-vision#$user"
