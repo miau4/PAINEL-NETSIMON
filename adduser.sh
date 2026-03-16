b36b004b46634df7abd845c2534ea957b752b9cb
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 05:39:05 -0300

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+CONFIG="/etc/xray/config.json"
+KEYFILE="/etc/xray-manager/reality.key"
+
 read -p "Nome do usuario: " user
 
 UUID=$(uuidgen)
 
 IP=$(curl -s ifconfig.me)
 
-CONFIG="/etc/xray/config.json"
+PUBLIC=$(grep PUBLIC $KEYFILE | cut -d= -f2)
+
+SNI="www.cloudflare.com"
+SID="6ba85179e30d4fc2"
 
 jq --arg id "$UUID" '.inbounds[0].settings.clients += [{"id":$id}]' $CONFIG > /tmp/config.json
 mv /tmp/config.json $CONFIG
@@ -19,7 +25,7 @@ echo ""
 echo "Usuario criado"
 echo ""
 
-echo "LINK:"
+echo "LINK VLESS REALITY:"
 echo ""
 
-echo "vless://$UUID@$IP:443?security=reality&type=tcp&sni=www.cloudflare.com&flow=xtls-rprx-vision#$user"
+echo "vless://$UUID@$IP:443?encryption=none&security=reality&type=tcp&sni=$SNI&fp=chrome&pbk=$PUBLIC&sid=$SID&flow=xtls-rprx-vision#$user"
