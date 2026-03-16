0019694672a5f5c6a7a7d706c9f17b73f070846f
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 04:57:39 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,32 +2,31 @@
 
 clear
 
-echo "XRAY MANAGER"
+echo "━━━━━━━━━━━━━━━━━━━━━━"
+echo " XRAY MANAGER MULT"
+echo "━━━━━━━━━━━━━━━━━━━━━━"
 
 echo "1 Criar usuario"
 echo "2 Remover usuario"
 echo "3 Usuarios online"
-echo "4 Rebuild config"
+echo "4 Definir limite"
+echo "5 Definir expiração"
+echo "6 Backup"
+echo "7 Rebuild config"
+echo "8 Verificar JSON"
 echo "0 Sair"
 
 read op
 
 case $op in
 
-1)
-adduser
-;;
-
-2)
-deluser
-;;
-
-3)
-online
-;;
-
-4)
-rebuild
-;;
+1) adduser ;;
+2) deluser ;;
+3) online ;;
+4) limit ;;
+5) expire ;;
+6) backup ;;
+7) rebuild ;;
+8) checkjson ;;
 
 esac
