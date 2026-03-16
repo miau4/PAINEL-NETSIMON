e62c1358e84f8e879624f46e26c2daff66859f5c
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 04:53:19 -0300

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+read -p "Nome: " user
+
+UUID=$(uuidgen)
+
+IP=$(curl -s ifconfig.me)
+
+jq --arg id "$UUID" '.inbounds[0].settings.clients += [{"id":$id}]' /etc/xray/config.json > /tmp/config.json
+
+mv /tmp/config.json /etc/xray/config.json
+
+systemctl restart xray
+
+echo ""
+echo "Usuario criado"
+echo ""
+
+echo "vless://$UUID@$IP:443?security=reality&type=tcp&sni=www.cloudflare.com&flow=xtls-rprx-vision#$user"
