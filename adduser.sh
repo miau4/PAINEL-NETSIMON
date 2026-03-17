5521a35054f2133affde30cd4930ca30f2e50a54
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 21:37:41 -0300

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,56 +1,21 @@
 #!/bin/bash
-CONFIG="/etc/xray/config.json"
+
 USERS="/etc/xray-manager/users.xray"
-KEYFILE="/etc/xray-manager/reality.key"
 
-# Criar arquivo de usuários se não existir
-[ ! -f "$USERS" ] && touch "$USERS"
-
-# Solicitar dados
 read -p "Nome do usuário: " user
-read -sp "Senha: " password
-echo ""
-read -p "Validade (dias): " validade
+read -p "Senha do usuário: " pass
+read -p "Dias de validade: " dias
 
-UUID=$(uuidgen)
-IP=$(curl -s ifconfig.me)
-PUBLIC=$(grep PUBLIC $KEYFILE | cut -d= -f2)
-SNI=$(jq -r '.inbounds[2].streamSettings.realitySettings.dest' $CONFIG)
-SID="6ba85179e30d4fc2"
+uuid=$(uuidgen)
+exp_date=$(date -d "+$dias days" +"%Y-%m-%d")
 
-# Calcular data de expiração
-EXP=$(date -d "+$validade days" +"%Y-%m-%d")
+echo "$user|$uuid|$exp_date|$pass" >> $USERS
 
-# Adicionar nos inbounds do Xray
-for i in 0 1 2 3; do
-  jq ".inbounds[$i].settings.clients += [{\"id\":\"$UUID\",\"email\":\"$user\"}]" $CONFIG > /tmp/config.json
-  mv /tmp/config.json $CONFIG
-done
-
-# Salvar no arquivo exclusivo
-echo "$user:$password:$UUID:$EXP" >> $USERS
-
-# Criar usuário SSH com a mesma senha
-useradd -M -s /bin/false "$user" 2>/dev/null
-echo "$user:$password" | chpasswd
-
-# Reiniciar Xray
-systemctl restart xray
-
-# Mostrar links
-echo -e "\n${user} criado com sucesso!"
-echo "Nome: $user"
-echo "Senha: $password"
-echo "UUID: $UUID"
-echo "Validade: $EXP"
-echo ""
-echo "LINK VLESS XHTTP:"
-echo "vless://$UUID@$IP:443?encryption=none&type=xhttp&security=tls&path=/#${user}"
-echo ""
-echo "LINK VLESS REALITY:"
-echo "vless://$UUID@$IP:443?encryption=none&security=reality&type=tcp&sni=$SNI&fp=chrome&pbk=$PUBLIC&sid=$SID&flow=xtls-rprx-vision#$user"
-
-if grep -q "^$user:" /etc/xray-manager/users.xray; then
-    echo "Erro: usuário já existe!"
-    exit 1
-fi
+echo "======================================"
+echo "Usuário criado com sucesso!"
+echo "Usuário: $user"
+echo "Senha: $pass"
+echo "UUID: $uuid"
+echo "Validade: $exp_date"
+echo "======================================"
+read -n1 -r -p "Pressione qualquer tecla para voltar..."
